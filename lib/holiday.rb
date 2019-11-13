5bbe5dc60162a3af066052caeb5cdb69faa4196f
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2019-11-13 16:11:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, values|
     values.each do |holiday, supplies_array|
+       binding.pry
       supplies_array.include?("BBQ")
-      binding.pry
+     
     end
   end
 end
